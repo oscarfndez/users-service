0ef23b452beb4a6e0dd9cacce161934342bfd05b
oscar <oscarfndez@gmail.com>
2026-04-25 00:21:19 +0200

    fix db name

--- src/main/java/com/oscarfndez/framework/config/SecurityConfiguration.java
+++ src/main/java/com/oscarfndez/framework/config/SecurityConfiguration.java
@@ -28,7 +28,9 @@ public class SecurityConfiguration {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(request -> request.requestMatchers("/api/v1/auth/**")
-                        .permitAll().anyRequest().authenticated())
+                        .permitAll()
+                        .requestMatchers("/api/auth/**").permitAll()
+                        .anyRequest().authenticated())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
                         jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
